1f6631cdd812394aa1977377c2b761128b9a7847
ilya <ilya.blan4@gmail.com>
2013-08-26 11:37:31 +0700

    fix speed of shadow and comrades.

--- src/entities/comrad.js
+++ src/entities/comrad.js
@@ -6,7 +6,7 @@ game.Comrad = me.ObjectEntity.extend({
         this.nickname = settings.nickname || "Charly";
 
         this.gravity = 0.0;
-        this.origVelocity = new me.Vector2d(5.0, 5.0);
+        this.origVelocity = new me.Vector2d(4.5, 4.5);
         this.setVelocity(this.origVelocity.x, this.origVelocity.y);
         this.setFriction(0.55, 0.55);
 
